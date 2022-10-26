e1f1a50de9f5a59f50ccebc8c76f44b6ce08aeb5
Antenor Pires <antenorpires.6@gmail.com>
2022-10-26 01:32:02 -0300

    feat: restart project

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,60 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import ConvertCurrency from './components/ConvertCurrency';
 
-const App = () => (
-  <>
-    <p>API:
-      <a href="https://economia.awesomeapi.com.br/json/last/USD-BRL">Link</a>
-    </p>
-    <ConvertCurrency
-      ofCurrency="USD"
-      forCurrency="BRL"
-    />
-    <ConvertCurrency
-      ofCurrency="BRL"
-      forCurrency="USD"
-    />
-  </>
-);
+const App = () => {
+  const currencies= ["BRL", "USD", "EUR"];
+  const [pair, setPair] = useState({ofCurrency: "", forCurrency: ""});
+
+  const handleOfSelect = (index, value) => {
+    if (index < 1) return;
+    setPair({ofCurrency: value, forCurrency: pair.forCurrency});
+  };
+  const handleForSelect = (index, value) => {
+    if (index < 1) return;
+    setPair({ofCurrency: pair.ofCurrency, forCurrency: value});
+  };
+
+  const renderSelectedPair = useMemo(() => {
+    if(pair.ofCurrency == pair.forCurrency) return;
+    return (
+      <ConvertCurrency
+        ofCurrency={pair.ofCurrency}
+        forCurrency={pair.forCurrency}
+      />
+    );
+  }, [pair]);
+
+  return (
+    <div>
+      <div>
+        <p>De:</p>
+        <select onClick={
+          (e) => handleOfSelect(e.target.selectedOptions[0].index, e.target.selectedOptions[0].text)
+        }>
+          <option value="0">Escolher Moeda</option>
+          {React.Children.toArray(
+            currencies.map(
+              (item, index) => <option value={index}>{item}</option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <p>Para:</p>
+        <select onClick={
+          (e) => handleForSelect(e.target.selectedOptions[0].index, e.target.selectedOptions[0].text)
+        }>
+          <option value="0">Escolher Moeda</option>
+          {React.Children.toArray(
+            currencies.map(
+              (item, index) => <option value={index}>{item}</option>
+          ))}
+        </select>
+      </div>
+      <div>
+        {renderSelectedPair}
+      </div>
+    </div>
+  );
+};
 
 export default App;
